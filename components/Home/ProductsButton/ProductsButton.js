a0b171dfd156881fbbc7b9eb0db13ac48f38ec15
Sharique Imam <shariquegithubweb@gmail.com>
2022-08-12 23:22:05 +0500

    asdf

--- components/Home/ProductsButton/ProductsButton.js
+++ components/Home/ProductsButton/ProductsButton.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 
 const style = {
-    wrapper: 'flex flex-col my-12',
+    wrapper: 'flex flex-col my-10 md:my-16',
     headingContainer: 'flex items-center jusitfy-center flex-col my-12',
     smallHeading: ' text-4xl md:text-4xl max-w-fit mx-auto py-2 text-stone-800 text-center',
-    btn: 'text-lg md:text-lg  px-12 md:px-24 py-4 md:py-4 max-w-fit mx-auto bg-[#c8a165] text-stone-100 hover:bg-opacity-[0.9] transition duration-[300ms]',
+    btn: 'text-lg md:text-lg  px-12 md:px-24 py-4 md:py-4 max-w-fit mx-auto bg-green-700 text-stone-100 hover:bg-opacity-[0.9] transition duration-[300ms]',
 }
 const ProductsButton = () => {
     return (
