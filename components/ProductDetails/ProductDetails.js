665ed684251d3aa25c539e7ea82d66f0ab15c18a
Sharique Imam <shariquegithubweb@gmail.com>
2022-08-13 18:48:28 +0500

    df

--- components/ProductDetails/ProductDetails.js
+++ components/ProductDetails/ProductDetails.js
@@ -145,8 +145,8 @@ const ProductDetails = ({
               <div className="hover:scale-[1.2] transition duration-[800ms] overflow-hidden">
                 <Image
                   src={`${urlForThumbnail(image)}`}
-                  height={width<700?190:260}
-                  width={width<700?280:340}
+                  height={width<700?200:260}
+                  width={width<700?270:340}
                   // className="hover:scale-[1.5] transition duration-[800ms] overflow-"
                 />
               </div>
