972f8e82457c5c2ea8cf449370e91062237d9996
Sharique Imam <shariquegithubweb@gmail.com>
2022-10-16 21:52:18 +0500

    adfg

--- components/SingleProduct/SingleProduct.js
+++ components/SingleProduct/SingleProduct.js
@@ -21,12 +21,14 @@ const SingleProduct = ({ item }) => {
       "font-bold px-3 py-6 bg-green-700 rounded-full cursor-pointer text-white",
     discoutnedPrice:
       " my-1 md:my-3 text-xs md:text-lg text-green-900 ml-4 font-bold",
+    freeDelivery: "text-white text-center mx-auto bg-green-700 px-5 py-0 md:py-1",
   };
   return (
     <NextLink href={`/${item.slug.current}`} passHref>
       <div className={style.wrapper} style={{ fontFamily: "Lato, sans-serif" }}>
         <div className="content">
           <div class="content-overlay"></div>
+
           <div className="content-image">
             <img
               src={`${urlForThumbnail(item.images[0])}`}
@@ -40,6 +42,12 @@ const SingleProduct = ({ item }) => {
           </div>
         </div>
         <h2 className={style.name}>{item.name.toUpperCase()}</h2>
+        {item.discount
+          ? (item.price - (item.price * item.discount) / 100).toFixed(0) >
+              1999 && <div className={style.freeDelivery}>Free Delivery</div>
+          : item.price > 1999 && (
+              <div className={style.freeDelivery}>Free Delivery</div>
+            )}
         {/* adding the discounted price */}
         <div className="flex items-center justify-center mx-auto my-1 md:mx-0">
           <h2 className={style.price}>Rs.{item.price}</h2>
@@ -52,7 +60,9 @@ const SingleProduct = ({ item }) => {
         {/* adding the discount value */}
         {item.discount && (
           <div className="flex items-center justify-center mx-auto md:mx-0 my-0 border-[2px] border-green-800 w-[100%] mb-2">
-            <h1 className="text-sm font-bold text-green-900">{item.discount}% off</h1>
+            <h1 className="text-sm font-bold text-green-900">
+              {item.discount}% off
+            </h1>
           </div>
         )}
 
